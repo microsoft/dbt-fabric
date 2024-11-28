19ff404636af93484910e702930cff70adb4134c
Pradeep Srikakolapu <prdpsvs@gmail.com>
2024-11-27 19:12:34 -0800

    Addressing #243, #221, #228, #229, #232, #235 issues

--- dbt/include/fabric/macros/adapters/show.sql
+++ dbt/include/fabric/macros/adapters/show.sql
@@ -1,16 +1,12 @@
 {% macro fabric__get_limit_sql(sql, limit) %}
-
-    {% if limit == -1 or limit is none %}
-        with model_limit_subq as (
-            {{ sql }}
-        )
-        select *
-        from model_limit_subq;
-    {% else -%}
-        with model_limit_subq as (
-            {{ sql }}
-        )
-        select top {{ limit }} *
-        from model_limit_subq;
+    {%- if limit == -1 or limit is none -%}
+        {{ sql }}
+    {#- Special processing if the last non-blank line starts with order by -#}
+    {%- elif 'order by' in sql.strip().splitlines()[-1].strip().lower() -%}
+        {{ sql }}
+        offset 0 rows  fetch first {{ limit }} rows only
+    {%- else -%}
+        {{ sql }}
+        order by (select null) offset 0 rows fetch first {{ limit }} rows only
     {%- endif -%}
 {% endmacro %}
