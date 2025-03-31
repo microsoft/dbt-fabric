7d9291503725d620e497a428f8d566dddb5fd728
Sam Debruyn <sam@debruyn.dev>
2025-03-31 03:47:19 +0200

    fix schema test

--- tests/functional/adapter/test_schema.py
+++ tests/functional/adapter/test_schema.py
@@ -26,11 +26,17 @@ select 1 as id
 select SCHEMA_OWNER from INFORMATION_SCHEMA.SCHEMATA where SCHEMA_NAME = '{schema_name}'
         """
         result = project.run_sql(get_schema_owner, fetch="one")[0]
-        assert result == owner
+        assert result == owner, f"Schema owner for {schema_name} is not {owner}"
 
     def test_schema_creation(self, project, unique_schema):
+        test_user = os.getenv("DBT_TEST_USER_1", "dbo")
+        if test_user != "dbo":
+            # Fabric only seems to recognize users as soon as you have granted them something
+            # You cannot grant something to dbo
+            project.run_sql(f"grant select on schema :: dbo to [{test_user}]", fetch=None);
+        
         res = run_dbt(["run"])
         assert len(res) == 1
-
-        self._verify_schema_owner(unique_schema, os.getenv("DBT_TEST_USER_1", "dbo"), project)
-        self._verify_schema_owner("with_custom_auth", os.getenv("DBT_TEST_USER_1", "dbo"), project)
+        
+        self._verify_schema_owner(unique_schema, test_user, project)
+        self._verify_schema_owner("with_custom_auth", test_user, project)
