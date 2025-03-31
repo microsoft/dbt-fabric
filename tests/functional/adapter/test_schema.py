9a867ddd88ef4b347b7f7f33048b056fb301fbf8
Sam Debruyn <sam@debruyn.dev>
2025-03-31 16:19:30 +0200

    initialize test user

--- tests/functional/adapter/test_schema.py
+++ tests/functional/adapter/test_schema.py
@@ -1,12 +1,17 @@
 import os
-from time import sleep
+from typing import Mapping
 
 import pytest
 
-from dbt.tests.util import run_dbt
+from dbt.adapters.fabric.fabric_adapter import FabricAdapter
+from dbt.tests.util import run_dbt, run_sql_with_adapter
 
 
 class TestSchemaCreation:
+    @pytest.fixture(scope="class")
+    def test_user(self):
+        return os.getenv("DBT_TEST_USER_1", "dbo")
+
     @pytest.fixture(scope="class")
     def models(self):
         return {
@@ -16,10 +21,16 @@ select 1 as id
 """,
         }
 
-    @staticmethod
     @pytest.fixture(scope="class")
-    def dbt_profile_target_update():
-        return {"schema_authorization": "{{ env_var('DBT_TEST_USER_1', 'dbo') }}"}
+    def dbt_profile_target_update(self, test_user: str):
+        return {"schema_authorization": f"{test_user}"}
+
+    @pytest.fixture(scope="class")
+    def initialization(initialization: None, adapter: FabricAdapter, test_user: str) -> None:
+        if test_user != "dbo":
+            # Fabric only seems to recognize users as soon as you have granted them something
+            # You cannot grant something to dbo
+            run_sql_with_adapter(adapter, f"grant select on schema :: dbo to [{test_user}]")
 
     @staticmethod
     def _verify_schema_owner(schema_name, owner, project):
@@ -34,13 +45,7 @@ where s.name = '{schema_name}'
         first_record = result[0]
         assert first_record == owner, f"Schema owner for {schema_name} is not {owner}"
 
-    def test_schema_creation(self, project, unique_schema):
-        test_user = os.getenv("DBT_TEST_USER_1", "dbo")
-        if test_user != "dbo":
-            # Fabric only seems to recognize users as soon as you have granted them something
-            # You cannot grant something to dbo
-            project.run_sql(f"grant select on schema :: dbo to [{test_user}]", fetch=None)
-
+    def test_schema_creation(self, project, unique_schema, test_user: str):
         res = run_dbt(["run"])
         assert len(res) == 1
 
