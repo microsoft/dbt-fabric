07db1102738adec4ad94b480808b3e45370f4cab
Sam Debruyn <sam@debruyn.dev>
2025-04-04 00:47:29 +0200

    hooks tests

---
+++ tests/functional/adapter/data/seed_model.sql
@@ -0,0 +1,16 @@
+drop table if exists {schema}.on_model_hook;
+
+create table {schema}.on_model_hook (
+    test_state       VARCHAR(100), -- start|end
+    target_dbname    VARCHAR(100),
+    target_host      VARCHAR(100),
+    target_name      VARCHAR(100),
+    target_schema    VARCHAR(100),
+    target_type      VARCHAR(100),
+    target_user      VARCHAR(100),
+    target_pass      VARCHAR(100),
+    target_threads   INT,
+    run_started_at   VARCHAR(100),
+    invocation_id    VARCHAR(100),
+    thread_id        VARCHAR(100)
+);
