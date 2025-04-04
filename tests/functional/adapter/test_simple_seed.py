c3b869bef752154a234ce63eea60d2327b6c5eca
Sam Debruyn <sam@debruyn.dev>
2025-04-05 00:30:25 +0200

    fix another test

--- tests/functional/adapter/test_simple_seed.py
+++ tests/functional/adapter/test_simple_seed.py
@@ -1,6 +1,6 @@
-import csv
-from pathlib import Path
 import inspect
+from pathlib import Path
+
 import pytest
 
 from dbt.tests.adapter.simple_seed import fixtures, seeds
@@ -19,9 +19,7 @@ from dbt.tests.adapter.simple_seed.test_seed import (
     BaseTestEmptySeed,
 )
 from dbt.tests.adapter.simple_seed.test_seed_type_override import BaseSimpleSeedColumnOverride
-from dbt.tests.util import (
-    copy_file,
-)
+from dbt.tests.util import copy_file, run_dbt
 
 fixed_seeds___expected_sql = (
     seeds.seeds__expected_sql.replace("TIMESTAMP WITHOUT TIME ZONE", "datetime2(6)")
@@ -32,8 +30,46 @@ fixed_properties__schema_yml = (
     fixtures.properties__schema_yml.replace("type: timestamp without time zone", "type: datetime2")
     .replace("type: text", "type: varchar")
     .replace("type: integer", "type: int")
-    .replace("type: boolean", "type: bit")
+    .replace("type: boolean", "type: int")
 )
+
+fixed_macros__schema_test = """
+{% test column_type(model, column_name, type) %}
+
+    {% set cols = adapter.get_columns_in_relation(model) %}
+
+    {% set col_types = {} %}
+    {% for col in cols %}
+        {% do col_types.update({col.name: col.data_type}) %}
+    {% endfor %}
+
+    {% set col_type = col_types.get(column_name) %}
+
+    {% set validation_message = 'Got a column type of ' ~ col_type ~ ', expected ' ~ type %}
+
+    {% set val = 0 if col_type and col_type.startswith(type) else 1 %}
+    {% if val == 1 and execute %}
+        {{ log(validation_message, info=True) }}
+    {% endif %}
+
+    select '{{ validation_message }}' as validation_error
+    from (select 1 as id) as nothing
+    where {{ val }} = 1
+
+{% endtest %}
+
+"""
+
+
+fixed_seeds__tricky_csv = """
+seed_id,seed_id_str,a_bool,looks_like_a_bool,a_date,looks_like_a_date,relative,weekday
+1,1,1,true,2019-01-01 12:32:30,2019-01-01 12:32:30,tomorrow,Saturday
+2,2,1,True,2019-01-01 12:32:31,2019-01-01 12:32:31,today,Sunday
+3,3,1,TRUE,2019-01-01 12:32:32,2019-01-01 12:32:32,yesterday,Monday
+4,4,0,false,2019-01-01 01:32:32,2019-01-01 01:32:32,tomorrow,Saturday
+5,5,0,False,2019-01-01 01:32:32,2019-01-01 01:32:32,today,Sunday
+6,6,0,FALSE,2019-01-01 01:32:32,2019-01-01 01:32:32,yesterday,Monday
+""".lstrip()
 
 
 class FixedSeedSetup:
@@ -83,15 +119,45 @@ class TestSeedWithUniqueDelimiterFabric(FixedSeedSetup, BaseSeedWithUniqueDelimi
 
 
 class TestSeedWithWrongDelimiterFabric(FixedSeedSetup, BaseSeedWithWrongDelimiter):
-    pass
+    def test_seed_with_wrong_delimiter(self, project):
+        """Testing failure of running dbt seed with a wrongly configured delimiter"""
+        seed_result = run_dbt(["seed"], expect_pass=False)
+        assert "Incorrect syntax near" in seed_result.results[0].message
 
 
 class TestSimpleSeedColumnOverrideFabric(BaseSimpleSeedColumnOverride):
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {
+            "seed_enabled.csv": seeds.seeds__enabled_in_config_csv,
+            "seed_disabled.csv": seeds.seeds__disabled_in_config_csv,
+            "seed_tricky.csv": fixed_seeds__tricky_csv,
+        }
+
     @pytest.fixture(scope="class")
     def models(self):
         return {
             "schema.yml": fixed_properties__schema_yml,
         }
+
+    @staticmethod
+    def seed_enabled_types():
+        return {
+            "seed_id": "varchar(100)",
+            "birthday": "date",
+        }
+
+    @staticmethod
+    def seed_tricky_types():
+        return {
+            "seed_id_str": "varchar(100)",
+            "looks_like_a_bool": "varchar(100)",
+            "looks_like_a_date": "varchar(100)",
+        }
+
+    @pytest.fixture(scope="class")
+    def macros(self):
+        return {"schema_test.sql": fixed_macros__schema_test}
 
 
 class TestSimpleSeedEnabledViaConfigFabric(BaseSimpleSeedEnabledViaConfig):
