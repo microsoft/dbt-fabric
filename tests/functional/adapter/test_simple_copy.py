9676fb99d5052f294908a9f1f033c85b4a69e72a
Sam Debruyn <sam@debruyn.dev>
2025-04-04 23:00:36 +0200

    fix test

--- tests/functional/adapter/test_simple_copy.py
+++ tests/functional/adapter/test_simple_copy.py
@@ -1,5 +1,6 @@
 from dbt.tests.adapter.simple_copy.test_copy_uppercase import BaseSimpleCopyUppercase
 from dbt.tests.adapter.simple_copy.test_simple_copy import EmptyModelsArentRunBase, SimpleCopyBase
+import pytest
 
 
 class TestEmptyModelsArentRunFabric(EmptyModelsArentRunBase):
@@ -7,8 +8,11 @@ class TestEmptyModelsArentRunFabric(EmptyModelsArentRunBase):
 
 
 class TestSimpleCopyBaseFabric(SimpleCopyBase):
-    pass
+    def test_simple_copy_with_materialized_views(self, project):
+        pytest.skip("Materialized views are not supported in this adapter.")
 
 
 class TestSimpleCopyUppercaseFabric(BaseSimpleCopyUppercase):
-    pass
+    @pytest.fixture(scope="class")
+    def dbt_profile_target(self, dbt_profile_target):
+        return dbt_profile_target
