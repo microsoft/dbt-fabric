201c83fa8e65d4ee1a3eacf2492a0f26eb606182
Sam Debruyn <sam@debruyn.dev>
2025-04-05 17:08:18 +0200

    simpler generate series

--- src/dbt/include/fabric/macros/dbt_package_support/dbt_utils/sql/generate_series.sql
+++ src/dbt/include/fabric/macros/dbt_package_support/dbt_utils/sql/generate_series.sql
@@ -1,33 +1,4 @@
 {% macro fabric__generate_series(upper_bound) %}
-
-    {% set n = get_powers_of_two(upper_bound) %}
-
-    with p as (
-        select 0 as generated_number union all select 1
-    ), unioned as (
-
-    select
-
-    {% for i in range(n) %}
-    p{{i}}.generated_number * power(2, {{i}})
-    {% if not loop.last %} + {% endif %}
-    {% endfor %}
-    + 1
-    as generated_number
-
-    from
-
-    {% for i in range(n) %}
-    p as p{{i}}
-    {% if not loop.last %} cross join {% endif %}
-    {% endfor %}
-
-    )
-
-    select *
-    from unioned
-    where generated_number <= {{upper_bound}}
-    {# in TSQL you can't have an order by in a view statement! #}
-    {# order by generated_number #}
-
+    select value as generated_number
+    from generate_series(1, {{ upper_bound }} - 1)
 {% endmacro %}
