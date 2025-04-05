cbea832aa301f65b2de9dcf8f30c6bf6148861c4
Sam Debruyn <sam@debruyn.dev>
2025-04-05 13:26:59 +0200

    skip array tests

--- tests/functional/adapter/utils/test_array_functions.py
+++ tests/functional/adapter/utils/test_array_functions.py
@@ -1,12 +1,16 @@
+import pytest
+
 from dbt.tests.adapter.utils.test_array_append import BaseArrayAppend
 from dbt.tests.adapter.utils.test_array_concat import BaseArrayConcat
 from dbt.tests.adapter.utils.test_array_construct import BaseArrayConstruct
 
 
+@pytest.mark.skip(reason="Array concat is not supported in Fabric")
 class TestArrayAppendFabric(BaseArrayAppend):
     pass
 
 
+@pytest.mark.skip(reason="Array concat is not supported in Fabric")
 class TestArrayConcatFabric(BaseArrayConcat):
     pass
 
