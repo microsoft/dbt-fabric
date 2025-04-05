293dbf8ff75e6e3d2c8b3755171a003f84cd71b6
Sam Debruyn <sam@debruyn.dev>
2025-04-05 16:49:29 +0200

    fix dateadd test

--- tests/functional/adapter/utils/test_dateadd.py
+++ tests/functional/adapter/utils/test_dateadd.py
@@ -1,5 +1,9 @@
+import pytest
+
 from dbt.tests.adapter.utils.test_dateadd import BaseDateAdd
 
 
 class TestDateAddFabric(BaseDateAdd):
-    pass
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {}
