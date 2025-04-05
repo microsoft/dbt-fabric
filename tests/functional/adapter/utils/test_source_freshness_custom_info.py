a8e63b5a30822005d3b33cd0f7784f326507a92c
Sam Debruyn <sam@debruyn.dev>
2025-04-05 15:11:09 +0200

    fix test

--- tests/functional/adapter/utils/test_source_freshness_custom_info.py
+++ tests/functional/adapter/utils/test_source_freshness_custom_info.py
@@ -1,5 +1,9 @@
+import pytest
+
 from dbt.tests.adapter.utils.test_source_freshness_custom_info import BaseCalculateFreshnessMethod
 
 
 class TestCalculateFreshnessMethodFabric(BaseCalculateFreshnessMethod):
-    pass
+    @pytest.fixture(scope="class")
+    def valid_sql(self) -> str:
+        return "select current_timestamp as c"
