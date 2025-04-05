0777760f8bfc0a2a84d3169332a998f48ab2435b
Sam Debruyn <sam@debruyn.dev>
2025-04-05 14:05:11 +0200

    remove invalid test

--- tests/functional/adapter/utils/test_current_timestamp.py
+++ tests/functional/adapter/utils/test_current_timestamp.py
@@ -1,15 +1,10 @@
 from dbt.tests.adapter.utils.test_current_timestamp import (
     BaseCurrentTimestamp,
-    BaseCurrentTimestampAware,
     BaseCurrentTimestampNaive,
 )
 
 
 class TestCurrentTimestampFabric(BaseCurrentTimestamp):
-    pass
-
-
-class TestCurrentTimestampAwareFabric(BaseCurrentTimestampAware):
     pass
 
 
