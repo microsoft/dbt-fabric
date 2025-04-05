faaa1dd454daa6a46da8ed44a1505486c003b48e
Sam Debruyn <sam@debruyn.dev>
2025-04-05 13:44:40 +0200

    Revert "remove tsql_utils namespace"
    This reverts commit cf9026c575c0d34fb90663ab516b6f64823cc172.

--- src/dbt/include/fabric/macros/dbt_package_support/dbt_utils/datetime/date_spine.sql
+++ src/dbt/include/fabric/macros/dbt_package_support/dbt_utils/datetime/date_spine.sql
@@ -1,125 +1,114 @@
-{% macro fabric__date_spine_sql(
-        datepart,
-        start_date,
-        end_date
-    ) %}
-    WITH l0 AS (
-        SELECT
-            C
-        FROM
-            (
-                SELECT
-                    1
-                UNION ALL
-                SELECT
-                    1
-            ) AS d(C)
+{% macro fabric__date_spine_sql(datepart, start_date, end_date) %}
+
+
+    with
+
+    l0 as (
+
+        select c
+        from (select 1 union all select 1) as d(c)
+
     ),
-    l1 AS (
-        SELECT
-            1 AS C
-        FROM
-            l0 AS A
-            CROSS JOIN l0 AS b
+    l1 as (
+
+        select
+            1 as c
+        from l0 as a
+        cross join l0 as b
+
     ),
-    l2 AS (
-        SELECT
-            1 AS C
-        FROM
-            l1 AS A
-            CROSS JOIN l1 AS b
+
+    l2 as (
+
+        select 1 as c
+        from l1 as a
+        cross join l1 as b
     ),
-    l3 AS (
-        SELECT
-            1 AS C
-        FROM
-            l2 AS A
-            CROSS JOIN l2 AS b
+
+    l3 as (
+
+        select 1 as c
+        from l2 as a
+        cross join l2 as b
     ),
-    l4 AS (
-        SELECT
-            1 AS C
-        FROM
-            l3 AS A
-            CROSS JOIN l3 AS b
+
+    l4 as (
+
+        select 1 as c
+        from l3 as a
+        cross join l3 as b
     ),
-    l5 AS (
-        SELECT
-            1 AS C
-        FROM
-            l4 AS A
-            CROSS JOIN l4 AS b
+
+    l5 as (
+
+        select 1 as c
+        from l4 as a
+        cross join l4 as b
     ),
-    nums AS (
-        SELECT
-            ROW_NUMBER() over (
-                ORDER BY
-                    (
-                        SELECT
-                            NULL
-                    )
-            ) AS ROWNUM
-        FROM
-            l5
+
+    nums as (
+
+        select row_number() over (order by (select null)) as rownum
+        from l5
     ),
-    rawdata AS (
-        SELECT
-            top ({{ dbt.datediff(start_date, end_date, datepart) }}) ROWNUM -1 AS n
-        FROM
-            nums
-        ORDER BY
-            ROWNUM
+
+    rawdata as (
+
+        select top ({{dbt.datediff(start_date, end_date, datepart)}}) rownum -1 as n
+        from nums
+        order by rownum
     ),
-    all_periods AS (
-        SELECT
-            (
-                {{ dbt.dateadd(
+
+    all_periods as (
+
+        select (
+            {{
+                dbt.dateadd(
                     datepart,
                     'n',
                     start_date
-                ) }}
-            ) AS date_ {{ datepart }}
-        FROM
-            rawdata
+                )
+            }}
+        ) as date_{{datepart}}
+        from rawdata
     ),
-    filtered AS (
-        SELECT
-            *
-        FROM
-            all_periods
-        WHERE
-            date_ {{ datepart }} <= {{ end_date }}
+
+    filtered as (
+
+        select *
+        from all_periods
+        where date_{{datepart}} <= {{ end_date }}
+
     )
-SELECT
-    *
-FROM
-    filtered
+
+    select * from filtered
+
 {% endmacro %}
 
-{% macro fabric__date_spine(
-        datepart,
-        start_date,
-        end_date
-    ) -%}
+
+{% macro fabric__date_spine(datepart, start_date, end_date) -%}
+
     {% set date_spine_query %}
-    {{ fabric__date_spine_sql(
-        datepart,
-        start_date,
-        end_date
-    ) }}
-ORDER BY
-    1 {% endset %}
+
+        {{tsql_utils.fabric__date_spine_sql(datepart, start_date, end_date)}} order by 1
+
+    {% endset %}
+
+
     {% set results = run_query(date_spine_query) %}
+
     {% if execute %}
-        {% set results_list = results.columns [0].values() %}
+
+    {% set results_list = results.columns[0].values() %}
+    
     {% else %}
-        {% set results_list = [] %}
+
+    {% set results_list = [] %}
+
     {% endif %}
 
     {%- for date_field in results_list %}
-    SELECT
-        '{{ date_field }}' AS date_ {{ datepart }}
-        {{ 'union all ' if not loop.last
-        ELSE '' }}
+        select '{{ date_field }}' as date_{{datepart}} {{ 'union all ' if not loop.last else '' }}
     {% endfor -%}
+
 {% endmacro %}
