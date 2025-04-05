071189bd7ff67953548182617df28118015dd030
Sam Debruyn <sam@debruyn.dev>
2025-04-05 23:03:36 +0200

    remove date_spine limitation

--- src/dbt/include/fabric/macros/utils/date_spine.sql
+++ src/dbt/include/fabric/macros/utils/date_spine.sql
@@ -1,14 +1,7 @@
 {% macro fabric__date_spine(datepart, start_date, end_date) %}
-    with rawdata as (
-
-        {{dbt.generate_series(
-            dbt.get_intervals_between(start_date, end_date, datepart)
-        )}}
-
-    ),
-
-    all_periods as (
-
+    
+    select *
+    from (
         select (
             {{
                 dbt.dateadd(
@@ -18,18 +11,12 @@
                 )
             }}
         ) as date_{{datepart}}
-        from rawdata
-
-    ),
-
-    filtered as (
-
-        select *
-        from all_periods
-        where date_{{datepart}} <= {{ end_date }}
-
-    )
-
-    select * from filtered
+        from ({{  
+                dbt.generate_series(
+                    dbt.get_intervals_between(start_date, end_date, datepart)
+                )
+            }}) raw_data
+    ) all_periods
+    where date_{{datepart}} <= {{ end_date }}
 
 {% endmacro %}
--- tests/functional/adapter/utils/test_date_spine.py
+++ tests/functional/adapter/utils/test_date_spine.py
@@ -1,13 +1,5 @@
-import pytest
-
 from dbt.tests.adapter.utils.test_date_spine import BaseDateSpine
 
 
 class TestDateSpineFabric(BaseDateSpine):
-    @pytest.fixture(scope="class")
-    def project_config_update(self):
-        return {
-            "models": {
-                "+materialized": "table"  # no support for nested CTEs in Views yet
-            },
-        }
+    pass
