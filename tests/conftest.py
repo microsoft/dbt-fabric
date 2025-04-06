55799beb655573f9cd552d828f42f5d839030c08
Sam Debruyn <sam@debruyn.dev>
2025-04-06 15:23:30 +0200

    fix pytest warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,6 @@ from pathlib import Path
 
 import pytest
 
-from dbt.tests.fixtures.project import TestProjInfo
-
 pytest_plugins = ["dbt.tests.fixtures.project"]
 
 
@@ -34,29 +32,6 @@ def profile_user(dbt_profile_target):
     return "dbo"
 
 
-class TestProjInfoFabric(TestProjInfo):
-    def get_tables_in_schema(self):
-        sql = f"""
-                select
-                        t.name as table_name,
-                        'table' as materialization
-                from sys.tables t
-                inner join sys.schemas s
-                on s.schema_id = t.schema_id
-                where lower(s.name) = '{self.test_schema.lower()}'
-                union all
-                select
-                        v.name as table_name,
-                        'view' as materialization
-                from sys.views v
-                inner join sys.schemas s
-                on s.schema_id = v.schema_id
-                where lower(s.name) = '{self.test_schema.lower()}'
-                """
-        result = self.run_sql(sql, fetch="all")
-        return {model_name: materialization for (model_name, materialization) in result}
-
-
 @pytest.fixture(scope="class")
 def logs_dir(request, prefix):
     dbt_log_dir = os.path.join(request.config.rootdir, "logs", prefix)
@@ -68,9 +43,33 @@ def logs_dir(request, prefix):
 
 @pytest.fixture(scope="class")
 def project(
-    project_setup: TestProjInfo,
+    project_setup,
     project_files,
 ):
+    from dbt.tests.fixtures.project import TestProjInfo
+
+    class TestProjInfoFabric(TestProjInfo):
+        def get_tables_in_schema(self):
+            sql = f"""
+                    select
+                            t.name as table_name,
+                            'table' as materialization
+                    from sys.tables t
+                    inner join sys.schemas s
+                    on s.schema_id = t.schema_id
+                    where lower(s.name) = '{self.test_schema.lower()}'
+                    union all
+                    select
+                            v.name as table_name,
+                            'view' as materialization
+                    from sys.views v
+                    inner join sys.schemas s
+                    on s.schema_id = v.schema_id
+                    where lower(s.name) = '{self.test_schema.lower()}'
+                    """
+            result = self.run_sql(sql, fetch="all")
+            return {model_name: materialization for (model_name, materialization) in result}
+
     return TestProjInfoFabric(
         project_root=project_setup.project_root,
         profiles_dir=project_setup.profiles_dir,
