55e34683fb4f008d0edb65fe643e477871dfae6f
Sam Debruyn <sam@debruyn.dev>
2025-04-07 15:50:03 +0200

    add dbt utils dispatching to every package

--- tests/functional/packages/base_package_test.py
+++ tests/functional/packages/base_package_test.py
@@ -17,17 +17,20 @@ class BaseDbtPackageTests:
         raise NotImplementedError("Subclasses must implement package_version")
 
     @pytest.fixture(scope="class")
-    def packages(self, package_repo: str, package_revision: str):
-        return {
-            "packages": [
-                {"git": package_repo, "revision": package_revision},
-                {
-                    "git": package_repo,
-                    "revision": package_revision,
-                    "subdirectory": "integration_tests",
-                },
-            ]
-        }
+    def packages(self, package_name: str, package_repo: str, package_revision: str):
+        packages = [
+            {"git": package_repo, "revision": package_revision},
+            {
+                "git": package_repo,
+                "revision": package_revision,
+                "subdirectory": "integration_tests",
+            },
+        ]
+
+        if package_name != "dbt_utils":
+            packages.append({"package": "dbt-labs/dbt_utils", "version": "1.3.0"})
+
+        return {"packages": packages}
 
     @pytest.fixture(scope="class")
     def models_config(self):
@@ -45,26 +48,38 @@ class BaseDbtPackageTests:
     def project_config_update(
         self, package_name: str, models_config: dict, seeds_config: dict, tests_config: dict
     ):
+        dispatches = [
+            {
+                "macro_namespace": package_name,
+                "search_order": [
+                    f"test_dbt_package",
+                    "dbt",
+                    package_name,
+                ],
+            }
+        ]
+        if package_name != "dbt_utils":
+            dispatches.append(
+                {
+                    "macro_namespace": "dbt_utils",
+                    "search_order": [
+                        f"test_dbt_package",
+                        "dbt",
+                        "dbt_utils",
+                    ],
+                }
+            )
+
         return {
             "name": "test_dbt_package",
             "vars": {"dbt_date:time_zone": "UTC"},
-            "dispatch": [
-                {
-                    "macro_namespace": package_name,
-                    "search_order": [
-                        f"test_dbt_package",
-                        "dbt_fabric",
-                        "dbt",
-                        package_name,
-                    ],
-                }
-            ],
+            "dispatch": dispatches,
             "seeds": seeds_config,
             "models": models_config,
             "tests": tests_config,
         }
 
-    def test_packages(self, project, dbt_core_bug_workaround):
+    def test_package(self, project, dbt_core_bug_workaround):
         run_dbt(["deps"])
         run_dbt(["seed"])
         run_dbt(["run"])
