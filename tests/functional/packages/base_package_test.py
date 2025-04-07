d0e551c5729b2ef689c2cdd554223a56c8bab807
Sam Debruyn <sam@debruyn.dev>
2025-04-07 14:57:44 +0200

    add a base class for package tests

---
+++ tests/functional/packages/base_package_test.py
@@ -0,0 +1,69 @@
+import pytest
+
+from dbt.tests.util import run_dbt
+
+
+class BaseDbtPackageTests:
+    @pytest.fixture(scope="class")
+    def package_name(self) -> str:
+        raise NotImplementedError("Subclasses must implement package_name")
+
+    @pytest.fixture(scope="class")
+    def package_repo(self) -> str:
+        raise NotImplementedError("Subclasses must implement package_repo")
+
+    @pytest.fixture(scope="class")
+    def package_revision(self) -> str:
+        raise NotImplementedError("Subclasses must implement package_version")
+
+    @pytest.fixture(scope="class")
+    def packages(self, package_repo: str, package_revision: str):
+        return {
+            "packages": [
+                {"git": package_repo, "revision": package_revision},
+                {
+                    "git": package_repo,
+                    "revision": package_revision,
+                    "subdirectory": "integration_tests",
+                },
+            ]
+        }
+
+    @pytest.fixture(scope="class")
+    def models_config(self):
+        return {}
+
+    @pytest.fixture(scope="class")
+    def seeds_config(self):
+        return {}
+
+    @pytest.fixture(scope="class")
+    def tests_config(self):
+        return {}
+
+    @pytest.fixture(scope="class")
+    def project_config_update(
+        self, package_name: str, models_config: dict, seeds_config: dict, tests_config: dict
+    ):
+        return {
+            "name": "test_dbt_package",
+            "dispatch": [
+                {
+                    "macro_namespace": package_name,
+                    "search_order": [
+                        f"test_dbt_package",
+                        "dbt_fabric",
+                        "dbt",
+                        package_name,
+                    ],
+                }
+            ],
+            "seeds": seeds_config,
+            "models": models_config,
+            "tests": tests_config,
+        }
+
+    def test_packages(self, project, dbt_core_bug_workaround):
+        run_dbt(["deps"])
+        run_dbt(["seed"])
+        run_dbt(["run"])
--- tests/functional/packages/test_dbt_utils.py
+++ tests/functional/packages/test_dbt_utils.py
@@ -1,9 +1,9 @@
 import pytest
 
-from dbt.tests.util import run_dbt
+from tests.functional.packages.base_package_test import BaseDbtPackageTests
 
 
-class TestDbtUtils:
+class TestDbtUtils(BaseDbtPackageTests):
     @pytest.fixture(scope="class")
     def macros(self):
         return {
@@ -14,53 +14,34 @@ class TestDbtUtils:
         }
 
     @pytest.fixture(scope="class")
-    def packages(self):
-        return {
-            "packages": [
-                {"git": "https://github.com/dbt-labs/dbt-utils", "revision": "1.3.0"},
-                {
-                    "git": "https://github.com/dbt-labs/dbt-utils",
-                    "revision": "1.3.0",
-                    "subdirectory": "integration_tests",
-                },
-            ]
-        }
+    def package_name(self) -> str:
+        return "dbt_utils"
 
     @pytest.fixture(scope="class")
-    def project_config_update(self):
-        return {
-            "name": "test_dbt_utils",
-            "dispatch": [
-                {
-                    "macro_namespace": "dbt_utils",
-                    "search_order": [
-                        "test_dbt_utils",
-                        "dbt_fabric",
-                        "dbt",
-                        "dbt_utils",
-                    ],
-                }
-            ],
-            "seeds": {
-                "dbt_utils_integration_tests": {
-                    "sql": {
-                        "data_get_single_value": {
-                            "+column_types": {"date_value": "datetime2(6)", "int_value": "int"}
-                        }
-                    },
-                    "schema_tests": {
-                        "data_test_sequential_timestamps": {
-                            "+column_types": {"my_timestamp": "datetime2(6)"}
-                        }
-                    },
-                }
-            },
-            "models": {
-                "dbt_utils_integration_tests": {"sql": {"test_groupby": {"+enabled": False}}}
-            },
-        }
+    def package_repo(self) -> str:
+        return "https://github.com/dbt-labs/dbt-utils"
 
-    def test_dbt_utils(self, project, dbt_core_bug_workaround):
-        run_dbt(["deps"])
-        run_dbt(["seed"])
-        run_dbt(["run"])
+    @pytest.fixture(scope="class")
+    def package_revision(self) -> str:
+        return "1.3.0"
+
+    @pytest.fixture(scope="class")
+    def models_config(self):
+        return {"dbt_utils_integration_tests": {"sql": {"test_groupby": {"+enabled": False}}}}
+
+    @pytest.fixture(scope="class")
+    def seeds_config(self):
+        return {
+            "dbt_utils_integration_tests": {
+                "sql": {
+                    "data_get_single_value": {
+                        "+column_types": {"date_value": "datetime2(6)", "int_value": "int"}
+                    }
+                },
+                "schema_tests": {
+                    "data_test_sequential_timestamps": {
+                        "+column_types": {"my_timestamp": "datetime2(6)"}
+                    }
+                },
+            }
+        }
