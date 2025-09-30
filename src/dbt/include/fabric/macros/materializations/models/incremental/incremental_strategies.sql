df1b5b6cec9e9febe5f795f1617d7a14f2c972dc
Sam Debruyn <sam@debruyn.dev>
2025-09-30 13:18:21 +0200

    support SQL MERGE

--- src/dbt/include/fabric/macros/materializations/models/incremental/incremental_strategies.sql
+++ src/dbt/include/fabric/macros/materializations/models/incremental/incremental_strategies.sql
@@ -1,10 +1,8 @@
 {% macro fabric__get_incremental_default_sql(arg_dict) %}
 
     {% if arg_dict["unique_key"] %}
-        -- Delete + Insert Strategy, calls get_delete_insert_merge_sql
-        {% do return(get_incremental_delete_insert_sql(arg_dict)) %}
+        {% do return(get_merge_sql(arg_dict)) %}
     {% else %}
-        -- Incremental Append will insert data into target table.
         {% do return(get_incremental_append_sql(arg_dict)) %}
     {% endif %}
 
