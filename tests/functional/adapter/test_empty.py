812e2d6b2fa27dfae86dce665407c459c39a65c4
Sam Debruyn <sam@debruyn.dev>
2025-10-04 22:45:18 +0200

    fix empty inline SQL test

--- tests/functional/adapter/test_empty.py
+++ tests/functional/adapter/test_empty.py
@@ -1,8 +1,20 @@
 from dbt.tests.adapter.empty.test_empty import BaseTestEmpty, BaseTestEmptyInlineSourceRef
+from dbt.tests.adapter.empty._models import schema_sources_yml
+import pytest
 
 
 class TestFabricEmpty(BaseTestEmpty):
     pass
 
 class TestFabricEmptyInlineSourceRef(BaseTestEmptyInlineSourceRef):
-    pass
+
+    model_inline_sql = """
+        select * from {{ source('seed_sources', 'raw_source') }} as raw_source
+        """
+    
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "model.sql": "select * from {{ source('seed_sources', 'raw_source') }}",
+            "sources.yml": schema_sources_yml,
+        }
