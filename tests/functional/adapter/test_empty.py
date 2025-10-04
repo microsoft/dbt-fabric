47f3ac74f91a28f02e250d9f497130feb9f68d8e
Sam Debruyn <sam@debruyn.dev>
2025-10-04 22:39:51 +0200

    fix empty test

--- tests/functional/adapter/test_empty.py
+++ tests/functional/adapter/test_empty.py
@@ -1,30 +1,8 @@
-import pytest
-
-from dbt.tests.adapter.empty import _models
 from dbt.tests.adapter.empty.test_empty import BaseTestEmpty, BaseTestEmptyInlineSourceRef
-
-model_sql = """
-select *
-from {{ ref('model_input') }} m1
-union all
-select *
-from {{ ref('ephemeral_model_input') }} m2 
-union all
-select *
-from {{ source('seed_sources', 'raw_source') }} m3
-"""
 
 
 class TestFabricEmpty(BaseTestEmpty):
-    @pytest.fixture(scope="class")
-    def models(self):
-        return {
-            "model_input.sql": _models.model_input_sql,
-            "ephemeral_model_input.sql": _models.ephemeral_model_input_sql,
-            "model.sql": model_sql,
-            "sources.yml": _models.schema_sources_yml,
-        }
-
+    pass
 
 class TestFabricEmptyInlineSourceRef(BaseTestEmptyInlineSourceRef):
     pass
