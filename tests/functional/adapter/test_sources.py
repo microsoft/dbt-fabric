a5a90ec52a4509d2ac3c7371f840c0d8300681ad
Sam Debruyn <sam@debruyn.dev>
2025-10-04 22:35:54 +0200

    update to test with dbt-core 1.10

--- tests/functional/adapter/test_sources.py
+++ tests/functional/adapter/test_sources.py
@@ -17,25 +17,8 @@ sources:
       - not_null
 """
 
-source_space_in_name = """
-version: 2
-sources:
-- name: 'space in name'
-  schema: {}
-  tables:
-  - name: sample
-    columns:
-    - name: id
-      tests:
-      - not_null
-"""
-
 select_from_source_regular = """
 select id from {{ source("regular", "sample") }}
-"""
-
-select_from_source_space_in_name = """
-select id from {{ source("space in name", "sample") }}
 """
 
 
@@ -44,25 +27,20 @@ class TestSourcesFabric:
     def models(self, unique_schema: str):
         return {
             "source_regular.yml": source_regular.format(unique_schema),
-            "source_space_in_name.yml": source_space_in_name.format(unique_schema),
             "v_select_from_source_regular.sql": config_materialized_view
             + select_from_source_regular,
-            "v_select_from_source_space_in_name.sql": config_materialized_view
-            + select_from_source_space_in_name,
             "t_select_from_source_regular.sql": config_materialized_table
-            + select_from_source_regular,
-            "t_select_from_source_space_in_name.sql": config_materialized_table
-            + select_from_source_space_in_name,
+            + select_from_source_regular
         }
 
-    def test_dbt_run(self, project):
+    def test_dbt_run(self, project: TestProjInfo):
         project.run_sql(f"create table {project.test_schema}.sample (id int)")
         run_dbt(["compile"])
 
         ls = run_dbt(["list"])
-        assert len(ls) == 8
+        assert len(ls) == 4
         ls_sources = [src for src in ls if src.startswith("source:")]
-        assert len(ls_sources) == 2
+        assert len(ls_sources) == 1
 
         run_dbt(["run"])
         run_dbt(["test"])
