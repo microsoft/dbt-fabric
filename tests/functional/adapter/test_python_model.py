3f1eb0ecfad7d8b97190ba651d7d64d1e95f4e37
Sam Debruyn <sam@debruyn.dev>
2025-10-05 10:55:32 +0200

    add skip markers for incremental Python models and PySpark tests

--- tests/functional/adapter/test_python_model.py
+++ tests/functional/adapter/test_python_model.py
@@ -11,9 +11,11 @@ class TestPythonModelTestsFabric(BasePythonModelTests):
     pass
 
 
+@pytest.mark.skip("Support for incremental Python models is not yet implemented")
 class TestPythonIncrementalTestsFabric(BasePythonIncrementalTests):
     pass
 
 
+@pytest.mark.skip("We only support PySpark DataFrames for now")
 class TestPySparkTestsFabric(BasePySparkTests):
     pass
