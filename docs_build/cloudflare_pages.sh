bdc83cfa2a59e312384a82ece4735f039c37b687
Sam Debruyn <sam@debruyn.dev>
2025-10-18 21:58:38 +0200

    remove swetrix

--- docs_build/cloudflare_pages.sh
+++ docs_build/cloudflare_pages.sh
@@ -6,6 +6,4 @@ cd ..
 
 uv run --only-group docs mkdocs build -d ./docs_build/site
 
-curl -sLo ./docs_build/site/t1.js "https://cdn.jsdelivr.net/gh/Swetrix/swetrix-js@latest/dist/swetrix.js"
-curl -sLo ./docs_build/site/t1.js.map "https://cdn.jsdelivr.net/gh/Swetrix/swetrix-js@latest/dist/swetrix.js.map"
-curl -sLo ./docs_build/site/t2.js "https://cloud.umami.is/script.js"
+curl -sLo ./docs_build/site/t.js "https://cloud.umami.is/script.js"
