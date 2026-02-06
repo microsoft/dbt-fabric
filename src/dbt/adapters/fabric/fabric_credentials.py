6a18ea77912659e949e3130f40239d796b67387a
Sam Debruyn <sam@debruyn.dev>
2026-02-06 21:43:10 +0100

    fix for nvarchar column types

--- src/dbt/adapters/fabric/fabric_credentials.py
+++ src/dbt/adapters/fabric/fabric_credentials.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Optional
 
 from dbt.adapters.contracts.connection import Credentials
 
@@ -7,30 +6,32 @@ from dbt.adapters.contracts.connection import Credentials
 @dataclass
 class FabricCredentials(Credentials):
     driver: str
-    host: Optional[str] = None
+    host: str | None = None
     database: str
     schema: str
-    UID: Optional[str] = None
-    PWD: Optional[str] = None
-    windows_login: Optional[bool] = False
-    trace_flag: Optional[bool] = False
-    tenant_id: Optional[str] = None
-    client_id: Optional[str] = None
-    client_secret: Optional[str] = None
-    access_token: Optional[str] = None
-    token_scope: Optional[str] = None
-    authentication: Optional[str] = "auto"
-    encrypt: Optional[bool] = True  # default value in MS ODBC Driver 18 as well
-    trust_cert: Optional[bool] = False  # default value in MS ODBC Driver 18 as well
+    UID: str | None = None
+    PWD: str | None = None
+    windows_login: bool | None = False
+    trace_flag: bool | None = False
+    tenant_id: str | None = None
+    client_id: str | None = None
+    client_secret: str | None = None
+    access_token: str | None = None
+    token_scope: str | None = None
+    authentication: str | None = "auto"
+    encrypt: bool | None = True  # default value in MS ODBC Driver 18 as well
+    trust_cert: bool | None = False  # default value in MS ODBC Driver 18 as well
     retries: int = 3
-    schema_authorization: Optional[str] = None
-    login_timeout: Optional[int] = 0
-    query_timeout: Optional[int] = 0
-    workspace_id: Optional[str] = None
-    workspace_name: Optional[str] = None
-    lakehouse_id: Optional[str] = None
-    lakehouse_name: Optional[str] = None
-    tenant_id: Optional[str] = None
+    schema_authorization: str | None = None
+    login_timeout: int | None = 0
+    query_timeout: int | None = 0
+    workspace_id: str | None = None
+    workspace_name: str | None = None
+    lakehouse_id: str | None = None
+    lakehouse_name: str | None = None
+    tenant_id: str | None = None
+    fabric_base_api_uri: str = "https://api.fabric.microsoft.com/v1"
+    powerbi_base_api_uri: str = "https://api.powerbi.com/v1.0"
 
     _ALIASES = {
         "user": "UID",
@@ -81,6 +82,8 @@ class FabricCredentials(Credentials):
             "login_timeout",
             "query_timeout",
             "trace_flag",
+            "fabric_base_api_uri",
+            "powerbi_base_api_uri",
         )
 
     @property
