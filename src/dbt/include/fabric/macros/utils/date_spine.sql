012ca5d65290832103ef500a4c9aa8b988ab92fb
Sam Debruyn <sam@debruyn.dev>
2026-02-20 18:04:08 +0100

    avoid unnamed column warning for get_intervals_between

--- src/dbt/include/fabric/macros/utils/date_spine.sql
+++ src/dbt/include/fabric/macros/utils/date_spine.sql
@@ -1,3 +1,21 @@
+{% macro fabric__get_intervals_between(start_date, end_date, datepart) -%}
+    {%- call statement('get_intervals_between', fetch_result=True) %}
+
+        select {{ dbt.datediff(start_date, end_date, datepart) }} as diff
+
+    {%- endcall -%}
+
+    {%- set value_list = load_result('get_intervals_between') -%}
+
+    {%- if value_list and value_list['data'] -%}
+        {%- set values = value_list['data'] | map(attribute=0) | list %}
+        {{ return(values[0]) }}
+    {%- else -%}
+        {{ return(1) }}
+    {%- endif -%}
+
+{%- endmacro %}
+
 {% macro fabric__date_spine(datepart, start_date, end_date) %}
     
     select *
