cccd66d60f8b34d88c2d008448d99a912023674d
Sam Debruyn <sam@debruyn.dev>
2026-02-28 09:55:13 +0100

    Add seeds fixture for LastDay tests in FabricSpark adapter

--- tests/fabricspark/adapter/utils/test_last_day.py
+++ tests/fabricspark/adapter/utils/test_last_day.py
@@ -1,5 +1,15 @@
+import pytest
+
 from dbt.tests.adapter.utils.test_last_day import BaseLastDay
+
+seeds__data_last_day_csv = """date_day,date_part,result
+2018-01-02,month,2018-01-31
+2018-01-02,quarter,2018-03-31
+2018-01-02,year,2018-12-31
+"""
 
 
 class TestLastDayFabricSpark(BaseLastDay):
-    pass
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {"data_last_day.csv": seeds__data_last_day_csv}
