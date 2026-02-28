4a5752b9402cb222d5fbb265a046b9750395cde2
Sam Debruyn <sam@debruyn.dev>
2026-02-28 09:54:15 +0100

    Remove unused BaseCurrentTimestamp class from current timestamp tests in FabricSpark adapter

--- tests/fabricspark/adapter/utils/test_current_timestamp.py
+++ tests/fabricspark/adapter/utils/test_current_timestamp.py
@@ -1,11 +1,6 @@
 from dbt.tests.adapter.utils.test_current_timestamp import (
-    BaseCurrentTimestamp,
     BaseCurrentTimestampNaive,
 )
-
-
-class TestCurrentTimestampFabricSpark(BaseCurrentTimestamp):
-    pass
 
 
 class TestCurrentTimestampNaiveFabricSpark(BaseCurrentTimestampNaive):
