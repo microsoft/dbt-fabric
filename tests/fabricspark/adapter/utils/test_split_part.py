d2cc7cb817e34c516c43ff045cafe2ea9ad0942d
Sam Debruyn <sam@debruyn.dev>
2026-02-28 09:56:01 +0100

    Add seeds fixture for split_part tests in TestSplitPartFabricSpark

--- tests/fabricspark/adapter/utils/test_split_part.py
+++ tests/fabricspark/adapter/utils/test_split_part.py
@@ -1,5 +1,15 @@
+import pytest
+
 from dbt.tests.adapter.utils.test_split_part import BaseSplitPart
+
+seeds__data_split_part_csv = """parts,split_on,result_1,result_2,result_3,result_4
+a|b|c,|,a,b,c,c
+1|2|3,|,1,2,3,3
+EMPTY|EMPTY|EMPTY,|,EMPTY,EMPTY,EMPTY,EMPTY
+"""
 
 
 class TestSplitPartFabricSpark(BaseSplitPart):
-    pass
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {"data_split_part.csv": seeds__data_split_part_csv}
